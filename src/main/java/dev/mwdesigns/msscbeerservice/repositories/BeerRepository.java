8e656e1ac9e1d03527bf0606461dcefe8ca6a23b
mwilliams <michael.williams705@live.com>
2021-12-24 11:12:49 -0500

    add beer repository

---
+++ src/main/java/dev/mwdesigns/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,11 @@
+package dev.mwdesigns.msscbeerservice.repositories;
+
+import dev.mwdesigns.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+
+}
